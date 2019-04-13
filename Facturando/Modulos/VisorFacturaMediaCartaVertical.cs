b036b3fe28199a7d2fe7d40397fc9cafd8536983
Simón Andrés Ariza Forero <ares.faas@gmail.com>
2019-04-13 12:57:49 -0500

    configuracion impresion mediacartavertical -factura y remision

--- Facturando/Modulos/VisorFacturaMediaCartaVertical.cs
+++ Facturando/Modulos/VisorFacturaMediaCartaVertical.cs
@@ -64,6 +64,28 @@ namespace Facturando.Modulos
             billPrintModelBindingSource3.DataSource = _bill.BillTaxes;
             billPrintModelBindingSource4.DataSource = _bill.BillText;
 
+            this.reportViewer1.PrinterSettings.PrinterName = System.Configuration.ConfigurationSettings.AppSettings["NormalPrinterName"].ToString();
+            this.reportViewer1.PrinterSettings.Copies = short.Parse(System.Configuration.ConfigurationSettings.AppSettings["NormalPrinterCopies"].ToString());
+            System.Drawing.Printing.PaperSize paperSize = new System.Drawing.Printing.PaperSize();
+            int marginTop = int.Parse(System.Configuration.ConfigurationSettings.AppSettings["NormalPrinterMargins"].ToString().Split(';')[0].Split(',')[1]);
+            int marginLeft = int.Parse(System.Configuration.ConfigurationSettings.AppSettings["NormalPrinterMargins"].ToString().Split(';')[1].Split(',')[1]);
+            int marginRight = int.Parse(System.Configuration.ConfigurationSettings.AppSettings["NormalPrinterMargins"].ToString().Split(';')[2].Split(',')[1]);
+            int marginBottom = int.Parse(System.Configuration.ConfigurationSettings.AppSettings["NormalPrinterMargins"].ToString().Split(';')[3].Split(',')[1]);
+                //System.Configuration.ConfigurationSettings.AppSettings["NormalPrinterMargins"].ToString().Split(';');
+            foreach (System.Drawing.Printing.PaperSize item in this.reportViewer1.PrinterSettings.PaperSizes)
+            {
+                if (item.PaperName.ToLowerInvariant().
+                    Equals(System.Configuration.ConfigurationSettings.AppSettings["NormalPaperName"].ToString().ToLowerInvariant()))
+                {
+                    paperSize = item;
+                }
+            }
+            this.reportViewer1.SetPageSettings(new System.Drawing.Printing.PageSettings {
+                PaperSize = paperSize,
+                Landscape = false,
+                Margins = new System.Drawing.Printing.Margins { Top = marginTop, Left = marginLeft, Right = marginRight, Bottom = marginBottom }
+            });
+
             this.reportViewer1.RefreshReport();
         }
 
